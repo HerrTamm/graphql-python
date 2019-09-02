6640dde0983935202d0af27804d70b90a56d323a
Rasmus Tamm <rasmus.tamm@ekspressdigital.eu>
2019-09-02 21:38:43 +0300

    Split seed data

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@
 pip install -r requirements.txt
 python manage.py makemigrations users
 python manage.py migrate
-python manage.py loaddata data.json
+python manage.py loaddata seeds/user.json
+python manage.py loaddata seeds/book.json
+python manage.py loaddata seeds/favouritebooks.json
 python manage.py runserver 0.0.0.0:8000
